07f55dfbb627b777f3f78b8b7432755c33ae809b
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-06 17:44:21 +0000

    Added erb file for templates

--- dice.rb
+++ dice.rb
@@ -9,11 +9,7 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 #dice program:
 get("/") do
-  '<h1>Dice Roll</h1>
-  <a href="/dice/2/6">Roll two 6-sided dice</a><br>
-  <a href="/dice/2/10">Roll two 10-sided dice</a><br>
-  <a href="/dice/2/8">Roll two 8-sided dice</a><br>
-'
+  erb(:elephant)
 end
 get("/zebra") do
   "We must add a route for each path we want to support"
